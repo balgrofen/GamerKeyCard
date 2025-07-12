87a651c608b96c12702202df4c17a834171f5955
balgrofen <gellencsabakalman1@gmail.com>
2025-07-12 08:14:34 +0200

    First commit. Drivers are working main.py and boot.py empty. readerTest.py tests the card reader. test.py shows the graphics on the screen

---
+++ readerTest.py
@@ -0,0 +1,42 @@
+# More details can be found in TechToTinker.blogspot.com 
+# George Bantique | tech.to.tinker@gmail.com
+
+from mfrc522 import MFRC522
+from machine import Pin
+from machine import SPI
+from machine import Pin, PWM
+from time import sleep_ms
+buzzer = Pin(33, Pin.OUT)
+buzzer.value(0)
+
+
+spi = SPI(2, baudrate=2500000, polarity=0, phase=0)
+# Using Hardware SPI pins:
+#     sck=18   # yellow
+#     mosi=23  # orange
+#     miso=19  # blue
+#     rst=4    # white
+#     cs=5     # green, DS
+# *************************
+# To use SoftSPI,
+# from machine import SOftSPI
+# spi = SoftSPI(baudrate=100000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+spi.init()
+rdr = MFRC522(spi=spi, gpioRst=4, gpioCs=5)
+
+def beep(duration_ms=200):
+    buzzer.value(1)  # Turn buzzer on
+    sleep_ms(duration_ms)
+    buzzer.value(0)  # Turn buzzer off
+    
+print("Place card")
+
+while True:
+    
+    (stat, tag_type) = rdr.request(rdr.REQIDL)
+    if stat == rdr.OK:
+        (stat, raw_uid) = rdr.anticoll()
+        if stat == rdr.OK:
+            card_id = "uid: 0x%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+            beep()
+            print(card_id)
